cc62c5708982792ac865fc31b9f835f4e991057e
HP <HP@DESKTOP-N8O4CH5>
2024-12-10 15:25:18 -0600

    modificacion en CBusca

--- src/crud/CBusca.java
+++ src/crud/CBusca.java
@@ -39,6 +39,46 @@ public class CBusca {
     return cnslt.buscarValor(consulta);
 }
       
+         public String buscaCuenta2(String curp) throws SQLException {
+    consulta = "SELECT id_cuenta FROM cuenta WHERE curp = '" + curp + "';";  
+    return cnslt.buscarValor(consulta);
+}
+      
+         
+             public String buscaEstado(String estado) throws SQLException {
+    consulta = "SELECT id_estado \n" +
+                "FROM estado \n" +
+                "WHERE nombreEstado = '" + estado + "';";  
+    return cnslt.buscarValor(consulta);
+}
+             
+    public String buscaNacionalidad(String Nacionalidad) throws SQLException {
+    consulta = "SELECT id_nacionalidad\n" +
+    "FROM nacionalidad\n" +
+    "WHERE nacionalidad = '" + Nacionalidad + "';";  
+    return cnslt.buscarValor(consulta);
+}
+           
+    public String buscaCivil(String estadoCivil) throws SQLException {
+    consulta = "SELECT id_civil\n" +
+    "FROM civil\n" +
+    "WHERE estadoCivil = '" + estadoCivil + "';";    
+    return cnslt.buscarValor(consulta);
+}
+   
+     
+      
+      public int obtenerUltimoId() throws SQLException {
+    consulta = "SELECT LAST_INSERT_ID();";  // Esto obtiene el último id insertado de la sesión.
+    return Integer.parseInt(cnslt.buscarValor(consulta));  // Devuelve el último id insertado.
+}
+      
+     public int buscaTipoCuenta(String tipoCuenta) throws SQLException {
+    consulta = "SELECT id_tipo_cuenta \n" +
+    "FROM tipo_cuenta \n" +
+    "WHERE tipo_cuenta =  '" + tipoCuenta + "';";    
+    return Integer.parseInt(cnslt.buscarValor(consulta));  // Devuelve el último id insertado.
+}
     
     
 }
